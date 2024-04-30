ad9425386624d98b76546f5ddaed04ed35db8afd
Jeff Hays <j.hays.devs@gmail.com>
2024-04-30 09:13:42 -0400

    initial commit 🐣

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import { useShoppingCart } from '../context/ShoppingCartContext'
+
+export function Navbar() {
+  const { openCart, cartQuantity } = useShoppingCart()
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        {cartQuantity > 0 && (
+          <Button
+            onClick={openCart}
+            style={{
+              width: "3rem",
+              height: "3rem",
+              position: "relative",
+            }}
+            variant="primary"
+            className="d-flex rounded-circle"
+          >
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="#fff">
+              <title>cart</title>
+              <path d="M12 29c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-1.657 1.343-3 3-3s3 1.343 3 3z"></path>
+              <path d="M32 29c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-1.657 1.343-3 3-3s3 1.343 3 3z"></path>
+              <path d="M32 16v-12h-24c0-1.105-0.895-2-2-2h-6v2h4l1.502 12.877c-0.915 0.733-1.502 1.859-1.502 3.123 0 2.209 1.791 4 4 4h24v-2h-24c-1.105 0-2-0.895-2-2 0-0.007 0-0.014 0-0.020l26-3.98z"></path>
+            </svg>
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: "white",
+                width: "1.5rem",
+                height: "1.4rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                transform: "translate(25%, 25%)",
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
+      </Container>
+    </NavbarBs>
+  )
+}
---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,65 @@
+import { Card } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import { useShoppingCart } from '../context/ShoppingCartContext'
+import { formatCurrency } from '../utilities/formatCurrency'
+
+type StoreItemProps = {
+  id: number
+  name: string
+  price: number
+  imgUrl: string
+}
+  
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart()
+  const quantity = getItemQuantity(id)
+
+  return (
+    <Card className="d-flex justify-content-end">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        width="100%"
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column justify-content-start">
+        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">{name}</Card.Title> {/* Display the item name */}
+        <Card.Text>Price: {formatCurrency(price)}</Card.Text> {/* Display the item price */}
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button
+              className="w-100"
+              onClick={() => increaseCartQuantity(id)}
+            >
+              + Add To Cart
+            </Button>
+          ) : <div
+                className="d-flex align-items-center flex-column"
+                style={{ gap: '.5rem' }}
+            >
+              <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+              <span className="fs-3">{quantity}</span> in cart
+              <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: '.5rem' }}
+              >
+                <Button
+                  onClick={() => removeFromCart(id)}
+                  variant="danger"
+                  size="sm"
+                >
+                  Remove
+                </Button>
+                </div>
+            </div>}
+        </div>
+      </Card.Body>
+    </Card>
+  )
+}
