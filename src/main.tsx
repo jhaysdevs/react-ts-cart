a019c2d0fe357ba069d3bb4652417dec4320d532
JF Hays <j.hays.devs@gmail.com>
2025-09-08 16:02:23 -0400

    Updaitng main.tsx to no longer include masonry.css

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import './styles/masonry.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
